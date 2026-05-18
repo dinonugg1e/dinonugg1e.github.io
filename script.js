87770ae347637e9c9a2c98decb5a87f635dcbfd4
dinonugg1e <abimali339@gmail.com>
2026-05-18 15:49:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
- let score = 0;
-    const scoreDisplay = document.getElementById('score');
-    const clickBtn = document.getElementById('click-btn');
 
-    clickBtn.addEventListener('click', () => {
-        score++;
-        scoreDisplay.innerText = score;
-    });
